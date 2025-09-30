8717f0dad582a8af6b10a55ae9b4fa80593d004e
Suganya <skmsuganya27@gmail.com>
2025-09-30 20:23:18 +0530

    Time: 1 ms (99.5%), Space: 45 MB (54.86%) - LeetHub

--- 3651-transformed-array/3651-transformed-array.java
+++ 3651-transformed-array/3651-transformed-array.java
@@ -3,27 +3,28 @@ class Solution {
        int n=nums.length;
        if(n == 0) return new int[0];
        int res[]=new int[n];
-    //    for(int i=0;i<n;i++){
-    //     int t=nums[i];
-    //     int pos;
-    //     if(t>0){
-    //       pos = (i+t) % n;
-    //     }
-    //     else {
-    //         pos = (i+n+t)%n;
-    //     }
-    //     res[i]=nums[pos];
-    //    }
-    //    return res; 
-    for (int i = 0; i < n; i++) {
-            int steps = nums[i];
-            // Safe modulo that never gives negative index
-            int newIndex = ((i + steps) % n + n) % n;
-            
-            res[i] = nums[newIndex];
+       for(int i=0;i<n;i++){
+        int t=nums[i];
+        int pos;
+        if(t>0){
+          pos = (i+t) % n;
         }
+        else {
+            
+            pos = ((i+t)%n +n)%n;
+        }
+        res[i]=nums[pos];
+       }
+       return res; 
+    // for (int i = 0; i < n; i++) {
+    //         int steps = nums[i];
+    //         // Safe modulo that never gives negative index
+    //         int newIndex = ((i + steps) % n + n) % n;
+            
+    //         res[i] = nums[newIndex];
+    //     }
         
-        return res;
+    //     return res;
 
     }
 }
