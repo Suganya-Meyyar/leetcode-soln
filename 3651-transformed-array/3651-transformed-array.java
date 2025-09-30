a59f99109273fe118b5ea7f7e5c25a032d7b742a
Suganya <skmsuganya27@gmail.com>
2025-09-30 20:17:45 +0530

    Time: 1 ms (99.5%), Space: 45 MB (54.86%) - LeetHub

---
+++ 3651-transformed-array/3651-transformed-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+       int n=nums.length;
+       if(n == 0) return new int[0];
+       int res[]=new int[n];
+    //    for(int i=0;i<n;i++){
+    //     int t=nums[i];
+    //     int pos;
+    //     if(t>0){
+    //       pos = (i+t) % n;
+    //     }
+    //     else {
+    //         pos = (i+n+t)%n;
+    //     }
+    //     res[i]=nums[pos];
+    //    }
+    //    return res; 
+    for (int i = 0; i < n; i++) {
+            int steps = nums[i];
+            // Safe modulo that never gives negative index
+            int newIndex = ((i + steps) % n + n) % n;
+            
+            res[i] = nums[newIndex];
+        }
+        
+        return res;
+
+    }
+}
