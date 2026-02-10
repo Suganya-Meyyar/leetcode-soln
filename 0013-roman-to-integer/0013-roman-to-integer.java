222858d71afe33195b61b9c4fa9ab223fc5493e3
Suganya <skmsuganya27@gmail.com>
2026-02-10 19:53:52 +0530

    Time: 2 ms (99.44%), Space: 46.4 MB (85.66%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int romanToInt(String s) {
+        int len = s.length();
+        int tot =0;
+        for(int i=0;i<len;i++){
+            int cur = func(s.charAt(i));
+            if( i+1<len && cur< func(s.charAt(i+1)) ){
+                tot-=cur;
+            }
+            else{
+                tot+=cur;
+            }
+           
+        }
+         return tot;
+    }
+        private int func(char c){
+            switch(c){
+                case 'I':
+                return 1;
+
+                case 'V':
+                return 5;
+
+                case 'X':
+                return 10;
+
+                case 'L':
+                return 50;
+
+                case 'C':
+                return 100;
+
+                case 'D':
+                return 500;
+
+                case 'M':
+                return 1000;
+
+                default:
+                return 0;
+
+            }
+        }
+    
+}
