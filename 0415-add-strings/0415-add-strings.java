fcbe17f3f01059fded05d10993dedd0feffa7e10
Suganya <skmsuganya27@gmail.com>
2026-04-16 11:44:04 +0530

    Time: 2 ms (98.87%), Space: 43.9 MB (53.79%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,13 +1,13 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int j = num1.length();
-        int i = num2.length();
+        int j = num1.length()-1;
+        int i = num2.length()-1;
         int carry =0;
         StringBuilder res = new StringBuilder();
 
-        while(i>0 || j>0 || carry !=0){
-            int dig1 = (j>0)? num1.charAt(j)-"0":"0";
-            int dig2 = (i>0)? num2.charAt(i)-"0":"0";
+        while(i>=0 || j>=0 || carry !=0){
+            int dig1 = (j>=0)? num1.charAt(j)-'0':0;
+            int dig2 = (i>=0)? num2.charAt(i)-'0':0;
             int sum = dig1 + dig2 + carry;
             res.append(sum%10);
             carry = sum/10;
