d108f4e3227f69c95b340be9179ee74ab1cb4904
Suganya <skmsuganya27@gmail.com>
2026-04-16 11:44:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int j = num1.length();
+        int i = num2.length();
+        int carry =0;
+        StringBuilder res = new StringBuilder();
+
+        while(i>0 || j>0 || carry !=0){
+            int dig1 = (j>0)? num1.charAt(j)-"0":"0";
+            int dig2 = (i>0)? num2.charAt(i)-"0":"0";
+            int sum = dig1 + dig2 + carry;
+            res.append(sum%10);
+            carry = sum/10;
+            i--;
+            j--;
+
+        }
+        return res.reverse().toString();
+    }
+}
