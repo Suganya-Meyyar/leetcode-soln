f8f009c6380dcca116b0bc590abe9279428e4390
Suganya <skmsuganya27@gmail.com>
2026-06-01 21:49:22 +0530

    Time: 6 ms (41.78%), Space: 44.9 MB (7.51%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+     Arrays.sort(cost);
+     int sum=0;
+     int len = cost.length;
+     for(int i=0;i<len;i++){
+        sum+=cost[i];
+     }
+     int count=0;
+     for(int i=len-1;i>=0;i--){
+        count++;
+        if(count==3){
+        count=0;
+        sum-=cost[i];
+        }
+     } 
+     return sum;
+    }
+}
